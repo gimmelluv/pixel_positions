39a18ee35e911e93943fc66f73b506567bb0dd96
Uliana Bulygina <ulanabulygina5@gmail.com>
2025-02-14 09:21:41 +0500

    тестовый коммит

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pixel Positions</title>
+    @Vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div>
+        <nav>
+            <div>
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/images/logo.svg')}}" alt="">
+                </a>
+            </div>
+            <div>
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
+            <div>
+                <a href="">Post a job</a>
+            </div>
+        </nav>
+
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+</html>
